420fcd5954e0839ef8b17348371b5ed74e044219
Jakub Radzik <jradzik4@gmail.com>
2022-07-17 17:34:38 +0200

    config

--- utils/JWT/jwt.js
+++ utils/JWT/jwt.js
@@ -1,28 +1,27 @@
-var dotenv = require('dotenv');
-dotenv.config();
 const jwt = require('jsonwebtoken');
+const { TOKEN_SECRET } = require('../config/config');
 
-const generateAccessToken = (username) => {
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '7d' });
-}
+const generateAccessToken = username => {
+  return jwt.sign(username, TOKEN_SECRET, { expiresIn: '7d' });
+};
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization'];
 
-  if (token == null) return res.sendStatus(401)
+  if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    console.log(err)
+  jwt.verify(token, TOKEN_SECRET, (err, user) => {
+    console.log(err);
 
-    if (err) return res.sendStatus(403)
+    if (err) return res.sendStatus(403);
 
-    req.user = user
+    req.user = user;
 
-    next()
-  })
-}
+    next();
+  });
+};
 
 module.exports = {
-    generateAccessToken,
-    authenticateToken
-}
+  generateAccessToken,
+  authenticateToken,
+};
